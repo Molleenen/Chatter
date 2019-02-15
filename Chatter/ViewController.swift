c8fda899df9d0195b888f81d0642c27f8e874611
Sebastian Wojciechowski <sebastian.wojciechowski.1998@gmail.com>
2019-02-15 11:40:02 +0100

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Robert Abramczyk on 15/02/2019.
+//  Copyright © 2019 Sebastian Wojciechowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
