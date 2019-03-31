b3a1aebc889e448ec623a193b841e57caa65480e
Sebastian Wojciechowski <sebastian.wojciechowski.1998@gmail.com>
2019-03-31 11:02:05 +0200

    Add tests for SocketService

--- Chatter/Model/Channel.swift
+++ Chatter/Model/Channel.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-struct Channel {
+struct Channel: Equatable {
     private(set) var identifier: String
     private(set) var name: String
     private(set) var description: String
